3fd0b95b7c84b3179734555f19c3b102c9c0fb52
SnayZz371 <vernatgabriel37@gmail.com>
2026-04-16 12:20:07 +0200

    réparation de admin

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -9,9 +9,9 @@ use Symfony\Component\Routing\Attribute\Route;
 #[Route('/admin', name: 'app_admin')]
 final class AdminController extends AbstractController
 {
-    #[Route('/dashboard', name: 'app_admin')]
+    #[Route('/', name: 'app_admin')]
     public function index(): Response
     {
-        return $this->render('admin/dashboard.html.twig' );
+        return $this->render('admin/admin.html.twig' );
     }
 }
