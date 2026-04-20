577b38a920de3270ebd945d06fed31f963049a65
SnayZz371 <vernatgabriel37@gmail.com>
2026-04-20 16:17:25 +0200

    Fixture

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use App\Factory\SoireeFactory;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        // $product = new Product();
+        // $manager->persist($product);
+        SoireeFactory::createMany(5);
+    }
+}
