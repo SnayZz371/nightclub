b129f1d919f08f1d34b2815130baaafcac66e831
SnayZz371 <vernatgabriel37@gmail.com>
2026-04-20 14:16:43 +0200

    CRUD

--- src/Form/Soiree1Type.php
+++ src/Form/Soiree1Type.php
@@ -7,7 +7,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class SoireeType extends AbstractType
+class Soiree1Type extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
