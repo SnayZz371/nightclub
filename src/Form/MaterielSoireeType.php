4c2ccc7de25dc7a0b1e7efb45b911edf9663e606
SnayZz371 <vernatgabriel37@gmail.com>
2026-04-21 11:08:09 +0200

    crud materielsoiree

---
+++ src/Form/MaterielSoireeType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Materiel;
+use App\Entity\MaterielSoiree;
+use App\Entity\Soiree;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class MaterielSoireeType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('dateReservationDebut')
+            ->add('dateReservationFin')
+            ->add('materiel', EntityType::class, [
+                'class' => Materiel::class,
+                'choice_label' => 'id',
+            ])
+            ->add('soiree', EntityType::class, [
+                'class' => Soiree::class,
+                'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => MaterielSoiree::class,
+        ]);
+    }
+}
